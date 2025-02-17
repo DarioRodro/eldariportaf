f17ea31defa3f941407ea8ddce201000bb8fe3f8
unknown <dariorodq30@gmail.com>
2025-02-17 00:00:30 -0500

    jej

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    base: '/eldariportaf',
     outDir: './dist' // Asegurar que la salida esté en "dist"
   });

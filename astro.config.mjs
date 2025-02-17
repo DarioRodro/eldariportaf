e99c2ee47cb50b4e625f7a1527c3037669e17c9c
unknown <dariorodq30@gmail.com>
2025-02-16 23:56:13 -0500

    siuu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '.',
-});
+    outDir: './dist' // Asegurar que la salida esté en "dist"
+  });

2438ed924a3b8ea6084ff32676459b4be982cfe5
unknown <dariorodq30@gmail.com>
2025-02-18 18:58:23 -0500

    Mi portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dariorodro.github.io/eldariportaf',
     base: '/eldariportaf',
     outDir: './dist' // Asegurar que la salida esté en "dist"
   });
